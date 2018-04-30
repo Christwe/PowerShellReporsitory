587627bee568334f7188f66e922669c7967cbf52
Chris W <christwe@microsoft.com>
2018-04-30 14:04:46 -0400

    Add files via upload

---
+++ Set-CdnOrigins.ps1
@@ -0,0 +1,31 @@
+﻿$TenantAdminUrl = "https://christwe-admin.sharepoint.com"
+$ReplicatedBaseTemplate = @("851","101")     #https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.listtemplatetype.aspx, https://www.codeproject.com/articles/765404/list-of-sharepoint-lists-basetemplatetype
+
+$Creds = Get-Credential
+Connect-SPOService -url $TenantAdminUrl -Credential $Creds
+
+Get-SPOSite | where {$_.Template -like "*PUBLISHING*"} | ForEach-Object {
+    $_.url
+    $context = New-Object Microsoft.SharePoint.Client.ClientContext($_.Url) 
+    $context.Credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($Creds.UserName, $Creds.Password)
+
+    $web = $context.Web
+    $lists = $web.Lists
+    $context.Load($web)
+    $context.Load($lists)
+    $context.ExecuteQuery()
+
+    #Must filter on Basetype = DocumentLibrary to insure we don't try to sync a list by accident
+    $Lists | where {$_.BaseType -eq "DocumentLibrary"} | ForEach-Object {
+        if($ReplicatedBaseTemplate -contains $_.BaseTemplate)
+        {
+            $_.Title
+            $ListFolder = $_.RootFolder
+            $context.Load($ListFolder)
+            $context.ExecuteQuery()
+            $LibUrl = $ListFolder.ServerRelativeUrl
+            Add-SPOTenantCdnOrigin -CdnType Private -OriginUrl $liburl -Confirm:$false
+        }
+    }
+}
+Disconnect-SPOService
